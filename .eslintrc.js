7ac1f3def31a65dd476379c491bcabfe9f5ad7ec
Tate Thurston <tatethurston@gmail.com>
2022-01-20 11:27:57 -0800

    Tate/update examples (#4)
    * Revert "migrate to eslint-plugin-react internals (#1)"
    
    This reverts commit 2f48be4cae6debda1b56760b09609816582a3e2c.
    
    * v1.0.0
    
    * update examples
    
    Co-authored-by: Tate <tate@transcriptic.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  ignorePatterns: ["dist", "examples"],
+  ignorePatterns: ["dist", "examples", "coverage"],
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
