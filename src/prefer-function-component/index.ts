b3dba98a475b2c874d840024479db532decc248a
Wolf <redenza@twitch.tv>
2022-06-23 12:02:03 -0400

    removes unused VARIABLE_DECLARATOR

--- src/prefer-function-component/index.ts
+++ src/prefer-function-component/index.ts
@@ -9,7 +9,6 @@ export const ALLOW_COMPONENT_DID_CATCH = "allowComponentDidCatch";
 const COMPONENT_DID_CATCH = "componentDidCatch";
 // https://eslint.org/docs/developer-guide/working-with-rules
 const PROGRAM_EXIT = "Program:exit";
-const VARIABLE_DECLARATOR = "VariableDeclarator";
 
 // TODO: Type definitions
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
