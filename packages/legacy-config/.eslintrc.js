fe0501288d363ca30c2919ac5c83ffada7293083
Tate Thurston <tatethurston@gmail.com>
2025-03-19 20:55:10 -0700

    add e2e tests

---
+++ packages/legacy-config/.eslintrc.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ["plugin:react-prefer-function-component/recommended"],
+};
---
+++ packages/legacy-config/index.jsx
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export class Foo extends Component {
+  render() {
+    return <div>{this.props.foo}</div>;
+  }
+}
